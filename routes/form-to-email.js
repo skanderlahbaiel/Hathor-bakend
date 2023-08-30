7fcf38184408929d6d68b56700146675417450d0
unknown <skander.lahbaiel@medtech.tn>
2023-08-30 18:17:58 +0100

    Socket server and regular http working

--- routes/form-to-email.js
+++ routes/form-to-email.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-//Define a route to handle form data and send emails
+
+// Define a route to handle form data and send emails
 router.post("/send-email", (req, res) => {
-  //Extract data from the body of the request
+  // Extract data from the body of the request
+  console.log(req.body)
   const fullname = req.body.fullname;
   const email = req.body.email;
   const time = req.body.time;
   const comments = req.body.comments;
   const bookingSubject = req.body.subject;
-  const textToSend = `Booking request received at ${Date()}\nName: ${fullname}\nE-mail: ${email}\nSubject:${bookingSubject}\nAt:${time}\nComments: ${comments}`
-  //Display data to console
+  const date = req.body.date
+  const textToSend = `Booking request received at ${Date()}\nName: ${fullname}\nE-mail: ${email}\nSubject:${bookingSubject}\nAt:${time}\nComments: ${comments}\n requested date: ${date}`;
+  // Display data to console
   console.log(textToSend);
+  // Emit a custom event to send data to WebSocket server
+  req.app.get("eventEmitter").emit("broadcast", req.body);
 
-  
-  //Return a response to the frontend
+  // Return a response to the frontend
   res.json({
-    message: "Sent sucessfully",
+    message: "Sent successfully",
   });
 });
 
--- server.js
+++ server.js
@@ -1,51 +1,54 @@
-
-
-const formToEmailRoute = require('./routes/form-to-email.js')
-const express = require('express');
-const http = require('http');
-const WebSocket = require('ws'); // Import the 'ws' library
+const EventEmitter = require("events");
+const formToEmailRoute = require("./routes/form-to-email.js");
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws"); // Import the 'ws' library
 
 const app = express();
+const eventEmitter = new EventEmitter();
+app.set("eventEmitter", eventEmitter);
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
+// Create a shared event emitter
 
-app.use('/booking', formToEmailRoute);
+app.use(express.json()); // Parse incoming JSON data
+app.use("/booking", formToEmailRoute);
 
+// Store connected clients (staff/baristas)
+const connectedClients = new Set();
 
 // Create a WebSocket server by passing in the HTTP server
 const wss = new WebSocket.Server({ server });
 
 // Define routes and middleware as needed for your app
-app.use(express.json());
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
 // Listen for WebSocket connections
-wss.on('connection', socket => {
-    console.log('A new client connected to WebSocket');
+wss.on("connection", (socket) => {
+  console.log("A new client connected to WebSocket");
 
-    // Handle WebSocket events
-    socket.on('message', message => {
-        console.log('Received message:', message);
-        socket.send('message received')
-        // Do something with the received message
+  // Handle WebSocket events
+  socket.on("message", (message) => {
+    console.log("Received message:", message);
+  });
+
+  // Listen for custom events to broadcast data
+  eventEmitter.on("broadcast", (data) => {
+    // Broadcast the data to connected clients
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(data));
+      }
     });
-
-    // You can also send messages to the client
-    socket.send('Hello, client! This is the server.');
+  });
 });
 
 // Start the combined HTTP and WebSocket server
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
-
-
-
-
-
-
-  
